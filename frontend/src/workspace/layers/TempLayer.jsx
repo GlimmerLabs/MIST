0e85f82f2b0293e7ad2299a7d592c1f0ce4485bc
lindavin <lindavin@grinnell.edu>
2020-12-20 13:03:20 -0500

    Format TempLayer

--- frontend/src/workspace/layers/TempLayer.jsx
+++ frontend/src/workspace/layers/TempLayer.jsx
@@ -21,31 +21,31 @@ import Edge from "../buildingTools/line";
 import PropTypes from "prop-types";
 
 function TempLayer({ tempLine }) {
-    return (<Layer>
-        {tempLine && (
-            <Edge
-                sourceX={tempLine.sourceX}
-                sourceY={tempLine.sourceY}
-                sinkX={tempLine.sinkX}
-                sinkY={tempLine.sinkY}
-                fill={tempLine.fill}
-                outletIndex={null}
-            />
-        )}
-    </Layer>)
+  return (<Layer>
+    {tempLine && (
+      <Edge
+        sourceX={tempLine.sourceX}
+        sourceY={tempLine.sourceY}
+        sinkX={tempLine.sinkX}
+        sinkY={tempLine.sinkY}
+        fill={tempLine.fill}
+        outletIndex={null}
+      />
+    )}
+  </Layer>)
 }
 
 TempLayer.propTypes = {
-    tempLine: PropTypes.oneOfType([
-        PropTypes.oneOf([null]),
-        PropTypes.shape({
-            sourceX: PropTypes.number.isRequired,
-            sourceY: PropTypes.number.isRequired,
-            sinkX: PropTypes.number.isRequired,
-            sinkY: PropTypes.number.isRequired,
-            fill: PropTypes.string.isRequired
-        })
-    ]).isRequired,
+  tempLine: PropTypes.oneOfType([
+    PropTypes.oneOf([null]),
+    PropTypes.shape({
+      sourceX: PropTypes.number.isRequired,
+      sourceY: PropTypes.number.isRequired,
+      sinkX: PropTypes.number.isRequired,
+      sinkY: PropTypes.number.isRequired,
+      fill: PropTypes.string.isRequired
+    })
+  ]).isRequired,
 }
 
 export default TempLayer;
